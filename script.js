d4b04ab059064b89e7ea8c07a2bc5c4c24fe093c
Diego Souza <diego-santos14@live.com>
2023-01-19 00:14:57 -0300

    Adicionado operções, variaveis imc e resultado e comparações de if

--- script.js
+++ script.js
@@ -6,15 +6,50 @@ function resultadoImc(){
 	var altura = parseInt(document.getElementById("altura").value);
 	var peso = parseInt(document.getElementById("peso").value);
 	var result = document.getElementById("result");
+	var situacao = document.getElementById("situacao");
 
-	imc = peso / (altura**2);
+	imc = peso / (altura**2)*10000;
 	
-	result.textContent = imc;
+	if (imc < 18.5) {
 
+		result.textContent = "Seu IMC É " + imc.toFixed(2);
+		situacao.textContent = "MAGREZA";
 
-	if (result >= 18.5){
+	}
 
-	document.getElementById("result").innerHTML = `Seu Imc é ${result}`;
+	else if (imc > 18.5 && imc <= 24.9) {
+
+		result.textContent = "Seu IMC É " + imc.toFixed(2);
+		situacao.textContent = "NORMAL";
 
 	}	
+
+	else if (imc >= 25 && imc <= 29.9) {
+
+		result.textContent = "Seu IMC É " + imc.toFixed(2);
+		situacao.textContent = "SOBREPESO";
+
+	}
+
+	else if (imc >= 30.0 && imc <= 39.9) {
+
+		result.textContent = "Seu IMC É " + imc.toFixed(2);
+		situacao.textContent = "OBESIDADE";
+
+	}			
+	
+	else if (imc > 40) {
+
+		result.textContent = "Seu IMC É " + imc.toFixed(2);
+		situacao.textContent = "OBESIDADE GRAVE";
+
+	}
+
+
+
+
+
+
+
+
 }
