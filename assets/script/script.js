b7532a65fb34606af9df161dffd55d396e40c804
Diego Souza <diego-santos14@live.com>
2023-02-01 22:29:42 -0300

    Adicionado comentários as condições

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,15 +2,15 @@
 
 function resultadoImc(){
 	
-	var imc = "";
-	var altura = parseInt(document.getElementById("altura").value);
-	var peso = parseInt(document.getElementById("peso").value);
-	var result = document.getElementById("result");
-	var situacao = document.getElementById("situacao");
+	let imc = "";
+	let altura = parseInt(document.getElementById("altura").value);
+	let peso = parseInt(document.getElementById("peso").value);
+	let result = document.getElementById("result");
+	const situacao = document.getElementById("situacao");
 
 	imc = peso / (altura**2)*10000; /* Adicionado o 10000 para que cortasse quatro zeros, sem isso, o resultado fica em numeros decimais */
 
-	if (imc < 18.5) {
+	if (imc < 18.5) { // Se for menor que 18 //
 
 		result.textContent = "Seu IMC É " + imc.toFixed(2);
 		situacao.textContent = "MAGREZA";
@@ -20,7 +20,7 @@ function resultadoImc(){
 
 	}
 
-	else if (imc > 18.5 && imc <= 24.9) {
+	else if (imc > 18.5 && imc <= 24.9) { // Se for maior a 18 e menor ou igual a 24.9 //
 
 		result.textContent = "Seu IMC É " + imc.toFixed(2);
 		situacao.textContent = "NORMAL";
@@ -30,7 +30,7 @@ function resultadoImc(){
 
 	}	
 
-	else if (imc >= 25 && imc <= 29.9) {
+	else if (imc >= 25 && imc <= 29.9) { // Se for maior ou igual a 25 e menor ou igual a 29.9 //
 
 		result.textContent = "Seu IMC É " + imc.toFixed(2);
 		situacao.textContent = "SOBREPESO";
@@ -40,7 +40,7 @@ function resultadoImc(){
 
 	}
 
-	else if (imc >= 30.0 && imc <= 39.9) {
+	else if (imc >= 30.0 && imc <= 39.9) { // Se for maior ou igual a 30 e menor ou igual a 39.9 //
 
 		result.textContent = "Seu IMC É " + imc.toFixed(2);
 		situacao.textContent = "OBESIDADE";
@@ -50,7 +50,7 @@ function resultadoImc(){
 
 	}			
 	
-	else if (imc > 40) {
+	else if (imc > 40) { // Se for maior que 40 //
 
 		result.textContent = "Seu IMC É " + imc.toFixed(2);
 		situacao.textContent = "OBESIDADE GRAVE";
